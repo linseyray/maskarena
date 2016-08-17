935fd1fffcbffd323f25b0cee703f78409d50486
Linsey R <Linsey R>
2016-08-17 22:35:37 +0200

    Converted ArenaController to GameManager

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ArenaController : MonoBehaviour {
+public class GameManager : MonoBehaviour {
 
 	public int nrPlayers;
 
