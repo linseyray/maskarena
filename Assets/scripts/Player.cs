050f59164ecad24cee8d6cec42777d43ba8b0f68
Linsey R <Linsey R>
2016-08-18 11:41:55 +0200

    Player state + Running/Idle animation

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,27 +4,51 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
+	public enum PlayerState { IDLE = 0, RUNNING = 1 };
+	private PlayerState currentState = PlayerState.IDLE;
+
 	private string name;
 	private int number;
 	public List<Color> colors;
 
+	private Animator animator;
+	private SpriteRenderer spriteRenderer;
 	private PlayerMovement playerMovement;
 
 	void Awake() {
 		playerMovement = gameObject.GetComponent<PlayerMovement>();
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		animator = gameObject.GetComponentInChildren<Animator>();
+
 	}
 
 	void Start () {
 	}
 	
-	// Update is called once per frame
 	void Update () {
-
 	}
 
 	public void SetNumber(int number) {
 		this.number = number;
 		playerMovement.SetupMovementLabels(number);
-		gameObject.GetComponent<SpriteRenderer>().color = colors[number-1];
+		spriteRenderer.color = colors[number-1];
+	}
+
+
+	public void GoToState(PlayerState newState) {
+		animator.SetInteger("state", (int) newState);
+
+		/*
+		switch (newState) {
+		case PlayerState.IDLE:
+			break;
+		case PlayerState.RUNNING:
+			break;
+		}
+		*/
+	}
+
+	public PlayerState GetCurrentState() {
+		return currentState;
 	}
 }
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// TODO rename to PlayerController
+
 public class PlayerMovement : MonoBehaviour {
 
+	private Player player;
 	private Rigidbody2D rigidBody2D;
+	public SpriteRenderer bodyRenderer;
+	public SpriteRenderer maskRenderer;
+
 	public float speed;
 
 	private string axisH;
 	private string axisV;
 
+	private bool facingLeft = false;
+
 	void Awake() {
 		rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+		player = gameObject.GetComponent<Player>();
 	}
 
 	void Start() {
@@ -30,5 +39,20 @@ public class PlayerMovement : MonoBehaviour {
 		float moveVertical = Input.GetAxis(axisV);
 		Vector2 inputDirection = new Vector2(moveHorizontal, moveVertical);
 		rigidBody2D.AddForce(inputDirection * speed);
+
+		if (!facingLeft && moveHorizontal < 0 || facingLeft && moveHorizontal > 0)
+			FlipSprite();
+
+		if (moveHorizontal == 0 && moveVertical == 0)
+			player.GoToState(Player.PlayerState.IDLE);
+		else
+		if (player.GetCurrentState() != Player.PlayerState.RUNNING)
+			player.GoToState(Player.PlayerState.RUNNING);
+	}
+
+	private void FlipSprite() {
+		facingLeft	= !facingLeft;
+		bodyRenderer.flipX = facingLeft;
+		//maskRenderer.flipX = facingRight;
 	}
 }
