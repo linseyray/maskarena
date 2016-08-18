ac20e22bf741e69bb509a209176d23470ffc9b21
carolineclark <carolinejaneclark@gmail.com>
2016-08-18 14:34:55 +0200

    Spawn players from fixed positions

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -23,7 +23,7 @@ public class GameManager : MonoBehaviour {
 
 	private void SpawnPlayers() {
 		for (int i = 0; i < nrPlayers; i++) {
-			GameObject player = objectSpawner.SpawnObjectRandom();
+			GameObject player = objectSpawner.SpawnObjectFixed (i);
 			player.GetComponent<Player>().SetNumber(i+1);
 			players.Add(player);
 		}
