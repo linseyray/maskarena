a28b678c8abbe3983e8274ea7068fe313ce86ca5
Linsey R <Linsey R>
2016-08-18 13:19:28 +0200

    Added dash

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -11,9 +11,11 @@ public class PlayerMovement : MonoBehaviour {
 	public SpriteRenderer maskRenderer;
 
 	public float speed;
+	public float dashSpeed;
 
 	private string axisH;
 	private string axisV;
+	private string dashButton;
 
 	private bool facingLeft = false;
 
@@ -28,26 +30,41 @@ public class PlayerMovement : MonoBehaviour {
 	public void SetupMovementLabels(int playerNumber) {
 		axisH = "P" + playerNumber + "_Horizontal";
 		axisV = "P" + playerNumber + "_Vertical";
+		dashButton = "P" + playerNumber + "_Dash";
 	}
 	
 	void Update () {
 		Move();
+
+		if (Input.GetButtonDown(dashButton))
+			Dash();
 	}
 
 	private void Move() {
 		float moveHorizontal = Input.GetAxis(axisH);
 		float moveVertical = Input.GetAxis(axisV);
 		Vector2 inputDirection = new Vector2(moveHorizontal, moveVertical);
+
 		rigidBody2D.AddForce(inputDirection * speed);
 
+		// Flip if necessary
 		if (!facingLeft && moveHorizontal < 0 || facingLeft && moveHorizontal > 0)
 			FlipSprite();
 
+		// Change animation states 
 		if (moveHorizontal == 0 && moveVertical == 0)
 			player.GoToState(Player.PlayerState.IDLE);
 		else
 		if (player.GetCurrentState() != Player.PlayerState.RUNNING)
 			player.GoToState(Player.PlayerState.RUNNING);
+	}
+
+	private void Dash() {
+		float moveHorizontal = Input.GetAxis(axisH);
+		float moveVertical = Input.GetAxis(axisV);
+		Vector2 inputDirection = new Vector2(moveHorizontal, moveVertical);
+
+		rigidBody2D.AddForce(inputDirection * dashSpeed, ForceMode2D.Impulse);
 	}
 
 	private void FlipSprite() {
