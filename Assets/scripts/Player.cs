2f343dc29e51ce269f48071a327ec5d008c673de
Simon Schliesky <schliesky.simon@gmail.com>
2016-08-18 08:27:03 +0200

    implements mask pickup and activation with player instance

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -15,6 +15,11 @@ public class Player : MonoBehaviour {
 	private SpriteRenderer spriteRenderer;
 	private PlayerMovement playerMovement;
 
+	public float defaultMaskTimeout = 5.0f;
+	private bool hasMask = false;
+	private float maskTimeLeft;
+	private Mask.TYPES maskType;
+
 	void Awake() {
 		playerMovement = gameObject.GetComponent<PlayerMovement>();
 		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
@@ -26,6 +31,13 @@ public class Player : MonoBehaviour {
 	}
 	
 	void Update () {
+		if(hasMask){
+			maskTimeLeft -= Time.deltaTime;
+			if(maskTimeLeft < 0) {
+				removeMask();
+			}
+		}
+
 	}
 
 	public void SetNumber(int number) {
@@ -50,5 +62,28 @@ public class Player : MonoBehaviour {
 
 	public PlayerState GetCurrentState() {
 		return currentState;
+	}
+
+	public void ReceiveMask(Mask.TYPES type){
+		this.maskType = type;
+		maskTimeLeft = defaultMaskTimeout;
+		// idea: if(type == Mask.TYPES.specialLongDurationMask) maskTimeLeft = 3*defaultMaskTimeout;
+		// TODO: add mask sprite to player object
+		hasMask = true;
+	}
+
+	private void removeMask(){
+		hasMask = false;
+		// TODO: remove mask sprite from player object
+	}
+
+	public void specialAction(){
+		if(hasMask) {
+			switch(maskType) {
+			default:
+				break;
+			}
+		}
+		
 	}
 }
